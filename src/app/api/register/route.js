24ea3ca84d88f6602e7a7dfdaa5dbd3f29a644e9
YounesFetouaki <younesfetouaki65@gmail.com>
2026-01-08 18:42:00 +0100

    fix: wrap entire register api in try-catch to handle connection errors

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,27 +4,27 @@ import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
-    const { name, email, password } = await request.json();
-
-    await dbConnect();
-
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-        return new NextResponse("Email is already in use", { status: 400 });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 5);
-    const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-    });
-
     try {
+        const { name, email, password } = await request.json();
+
+        await dbConnect();
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return new NextResponse(JSON.stringify({ error: "Email is already in use" }), { status: 400 });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 5);
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
         await newUser.save();
         return new NextResponse("User has been created", {
-            status: 200, // Fixed status
+            status: 200,
         });
     } catch (err) {
         console.error("Registration Error:", err);
